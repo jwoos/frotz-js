3465a2fb33c3affab37945c5482975fe790bbadf
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-08-02 01:19:41 -0400

    resolves #3

--- test/index.spec.js
+++ test/index.spec.js
@@ -54,9 +54,192 @@ describe('Class: DFrotzInterface', () => {
         });
     });
 
-    describe('Method: filter', () => {});
+    describe('Method: filter', () => {
+		it('should filter string starting with >', () => {
+			let arr = [
+				'first',
+				'second',
+				'>third',
+			];
 
-    describe('Method: command', () => {});
+			let result = arr.filter(DFrotzInterface.filter);
+
+			expect(result).toEqual([
+				'first',
+				'second'
+			]);
+		});
+
+		it('should filter out empty strings', () => {
+			let arr = [
+				'',
+				'',
+				'',
+				''
+			];
+
+			expect(arr.filter(DFrotzInterface.filter)).toEqual([]);
+		});
+
+		it('should keep the first elem if length is three', () => {
+			let arr = [
+				'first',
+				'',
+				''
+			];
+
+			expect(arr.filter(DFrotzInterface.filter)).toEqual([
+				'first'
+			]);
+		});
+	});
+
+	describe('Method: stripWhiteSpace', () => {
+		it('should strip whitespace', () => {
+			let str = '  asdasd    asdasd qeq';
+
+			let result = DFrotzInterface.stripWhiteSpace(str, true, true);
+
+			expect(result).toEqual('asdasd | asdasd qeq');
+		});
+
+		it('should do nothing if no arguments are specified', () => {
+			let str = '               asd ';
+			let result = DFrotzInterface.stripWhiteSpace(str);
+
+			expect(str).toEqual(result);
+		});
+
+		it('should only do outer', () => {
+			let str = '   asd   asd   ';
+			let result = DFrotzInterface.stripWhiteSpace(str, false, true);
+
+			expect(result).toEqual('asd   asd');
+		});
+	});
+
+    describe('Method: command', () => {
+		let frotz;
+		let mockDefer;
+
+		beforeEach(() => {
+			frotz = new DFrotzInterface();
+			frotz.dfrotz = {
+				stdin: {
+					write: () => {}
+				}
+			};
+
+			mockDefer = q.defer();
+
+			spyOn(frotz.dfrotz.stdin, 'write');
+		});
+
+		it('should write to stdin', () => {
+			frotz.command('a command');
+
+			expect(frotz.dfrotz.stdin.write).toHaveBeenCalled();
+		});
+
+		it('should work with promises', () => {
+			spyOn(q, 'defer').and.returnValues(mockDefer);
+			spyOn(mockDefer.promise, 'delay').and.callThrough();
+
+			let result = frotz.command('command');
+
+			expect(mockDefer.promise.delay).toHaveBeenCalledWith(10);
+		});
+	});
+
+	describe('Method: checkForSaveFile', () => {
+		let frotz;
+
+		beforeEach(() => {
+			frotz = new DFrotzInterface();
+		});
+
+		it('should return a promise', () => {
+			expect(frotz.checkForSaveFile().then).not.toBeNull();
+		});
+
+		it('should always resolve', () => {
+			frotz.checkForSaveFile().then((val) => {
+				expect(val).toEqual(false);
+			});
+
+			frotz.saveFile = './';
+
+			frotz.checkForSaveFile().then((val) => {
+				expect(val).toEqual(true);
+			});
+		});
+	});
+
+	describe('Method: restoreSave', () => {
+		let frotz;
+
+		beforeEach(() => {
+			frotz = new DFrotzInterface();
+			frotz.saveFile = './';
+
+			spyOn(frotz, 'command');
+			spyOn(q, 'all').and.callThrough();
+		});
+
+		it('should call restore', () => {
+			frotz.restoreSave(true);
+
+			expect(frotz.command).toHaveBeenCalledTimes(2);
+			expect(frotz.command).toHaveBeenCalledWith('restore');
+			expect(frotz.command).toHaveBeenCalledWith('./');
+		});
+
+		it('should return a promise', () => {
+			let result = frotz.restoreSave();
+
+			expect(result.then).not.toBeNull();
+		});
+
+		it('should consolidate promises', () => {
+			frotz.restoreSave();
+
+			expect(q.all).toHaveBeenCalled();
+		});
+	});
+
+	describe('Method: writeSave', () => {
+		let frotz;
+
+		beforeEach(() => {
+			frotz = new DFrotzInterface();
+			frotz.saveFile = './';
+
+			spyOn(frotz, 'command');
+			spyOn(q, 'all').and.callThrough();
+		});
+
+		it('should return a promise', () => {
+			let result = frotz.writeSave();
+
+			expect(result.then).not.toBeNull();
+		});
+
+		it('should call commands', () => {
+			frotz.writeSave();
+
+			expect(frotz.command).toHaveBeenCalledTimes(6);
+
+			for (let cmd of ['save', './', 'Y', 'quit', 'Y', 'SI']) {
+				expect(frotz.command).toHaveBeenCalledWith(cmd);
+			}
+		});
+
+		it('should consolidate promises', () => {
+			frotz.writeSave();
+
+			expect(q.all).toHaveBeenCalled();
+		});
+	});
 
     describe('Method: iteration', () => {});
 });
